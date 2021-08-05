bd0c38f6e95030872402bde1488afb8cfd57af8a
Clarissa P <clarissapamonicutt@gmail.com>
2021-08-04 20:01:04 -0700

    lab 06 complete

--- src/App.js
+++ src/App.js
@@ -1,29 +1,78 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 import './index.js'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Alert from 'react-bootstrap/Alert';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      cityName: true,
+      display_name: '',
+      city: '',
+      lat: '',
+      lon: '',
+      displayMap: false,
+      renderError: false,
+      errorMessage: '',
     }
+  }
+
+  // gets location info from api
+  getLocation = async (e) => {
+    //prevents form input from disappearing
+    e.preventDefault();
+    try {
+      let location = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json`)
+
+      this.setState({
+        lat: location.data[0].lat,
+        lon: location.data[0].lon,
+        display_name: location.data[0].display_name,
+        displayMap: true,
+      });
+
+      //check demo code for error handling, put in bootstrap compt
+    } catch (error) {
+      console.log(error.response);
+      this.setState({
+        renderError: true,
+        errorMessage: `Error: ${error.response.status}, Status: ${error.response.data.error}`,
+      });
+    }
+  }
+
+  // event handler for form(city) submission
+  handleChange = (event) => {
+    this.setState({ city: event.target.value });
+
   }
 
   render() {
     return (
       <main>
-        <Form>
+        <Form onSubmit={this.getLocation}>
           <Form.Group>
-          <Form.Label>City Name</Form.Label>
-          <Form.Control></Form.Control>
-          <Button>Explore!</Button>
+            <Form.Label>Enter a City Name</Form.Label>
+            <Form.Control onChange={this.handleChange}></Form.Control>
+            <Button>Explore!</Button>
           </Form.Group>
         </Form>
+        <Container>
+          <p>{this.state.display_name}</p>
+          <p>{this.state.lat}</p>
+          <p>{this.state.lon}</p>
+          {this.state.displayMap ? <Image src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.lat},${this.state.lon}&zoom=12`} alt='map' /> : ''}
+        </Container>
+        <Alert variant="danger" onClose={() => this.setState({renderError: false})} show={this.state.renderError} dismissible>
+          {this.state.errorMessage}
+        </Alert>
       </main>
     )
   }
@@ -31,6 +80,4 @@ class App extends React.Component {
 
 
 export default App;
-
-
-// Build a form to collect a city name from the user. Give the submit button the text of "Explore!"
+//NOTE: ADD CSS
